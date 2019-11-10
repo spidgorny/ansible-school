0f0d84f548949833474f1b5a28b40292ebf87c90
depidsvy <spidgorny@gmail.com>
2019-11-11 00:04:57 +0100

    Add veyon repo

---
+++ copy-files.sh
@@ -0,0 +1,1 @@
+rsync -r Mathe-Lernvideos user@192.168.1.172:Desktop -P
--- runme.sh
+++ runme.sh
@@ -2,7 +2,12 @@
 # if ! $ip then
   # return
 
+set -e
+set -x
+
+ip=$1
+sudo=$2
 ssh-copy-id -i id_rsa adminer@$ip
-ansible-playbook -i hostlist playbook-apt.yaml -K
+ansible-playbook -i hostlist playbook-apt.yaml -v --extra-vars "ansible_become_pass='$sudo'"
 ssh-copy-id -i id_rsa user@$ip
-ansible-playbook -i hostlist user.yaml
+ansible-playbook -i hostlist user.yaml -v
